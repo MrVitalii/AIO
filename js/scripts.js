a2ba6c23a76d56670480f699e1ebb53e2f995d82
MrVitalii <po4tavitaliya007@gmail.com>
2022-09-28 02:13:34 +0300

    Update 2

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+        lazyLoad: true,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        autoplaySpeed: 3500,
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1024:{
+                items:1
+            }
+        }
+    });
+});
